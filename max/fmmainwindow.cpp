4e59274311b17daf7328dd140ba5d17c4409fa99
Apex <apex@quixel.net>
2026-06-10 14:27:06 -0300

    typo

--- max/fmmainwindow.cpp
+++ max/fmmainwindow.cpp
@@ -3201,7 +3201,7 @@ void FMMainWindow::exportManufacturerColors(const QString& path)
     QJsonObject metadata_object;
     metadata_object.insert("version", 1);
     metadata_object.insert("type", "ManufacturerColors");
-    metadata_object.insert("generator", "ForzaTech for Autodesk 3ds Max");
+    metadata_object.insert("generator", "ForzaTech Importer for Autodesk 3ds Max");
 
     root_json_object.insert("metadata", metadata_object);
 
